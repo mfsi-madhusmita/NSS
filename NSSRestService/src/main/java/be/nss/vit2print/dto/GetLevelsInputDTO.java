119b83debe28aa4acdb8576ad3c331f52d0ccd1e
nishant <nishant@nishanth>
2015-05-22 19:31:07 +0530

    added pattern validation on libraryId

--- NSSRestService/src/main/java/be/nss/vit2print/dto/GetLevelsInputDTO.java
+++ NSSRestService/src/main/java/be/nss/vit2print/dto/GetLevelsInputDTO.java
@@ -2,15 +2,21 @@ package be.nss.vit2print.dto;
 
 import static be.nss.vit2print.exception.ExceptionMessages.INVALID_LIBRARY_ID;
 import static be.nss.vit2print.exception.ExceptionMessages.INVALID_USERNAME;
+import static be.nss.vit2print.exception.ExceptionMessages.INVALID_LIBRARY_ID_PATTERN;
+
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.NotBlank;
 
 public class GetLevelsInputDTO {
 
+	private static final String LIBRARY_ID_PATTERN = "[0-9]*[_]*[0-9]+$";
+
 	@NotBlank(message = INVALID_USERNAME)
 	private String username;
 
 	@NotBlank(message = INVALID_LIBRARY_ID)
+	@Pattern(regexp = LIBRARY_ID_PATTERN, message = INVALID_LIBRARY_ID_PATTERN)
 	private String libraryId;
 
 	/*
