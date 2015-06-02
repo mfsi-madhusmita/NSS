8e34a79232b82691455001996507960ad24446a0
nishant <nishant@nishanth>
2015-06-02 14:07:31 +0530

    updates in ValidationPatterns

--- NSSRestService/src/main/java/be/nss/vit2print/validation/ValidationPatterns.java
+++ NSSRestService/src/main/java/be/nss/vit2print/validation/ValidationPatterns.java
@@ -3,4 +3,5 @@ package be.nss.vit2print.validation;
 public interface ValidationPatterns {
 
 	String ASSET_ID_OR_LIBRARY_ID_PATTERN = "^[0-9]*[_]*[0-9]+$";
+	String ASSET__ID_PATTERN = "^[0-9]*[_]+[0-9]+$";
 }
